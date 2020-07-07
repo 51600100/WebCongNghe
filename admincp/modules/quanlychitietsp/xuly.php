751620165eb75c2abb21792ee8356eae913cc292
51600100 <43369623+rongcon973@users.noreply.github.com>
2020-07-07 14:22:01 +0700

    Add files via upload

---
+++ admincp/modules/quanlychitietsp/xuly.php
@@ -0,0 +1,34 @@
+<?php 
+	include('../config.php');
+	$id=$_GET['id'];
+	$tensp=$_POST['tensp'];
+	$mota=$_POST['motasp'];
+	$gia=$_POST['gia'];
+	$loaisp=$_POST['loaisp'];
+	$hinhanh=$_FILES['hinhanh']['name'];
+	$hinhanh_tmp=$_FILES['hinhanh']['tmp_name'];
+	move_uploaded_file($hinhanh_tmp,'uploads/'.$hinhanh);
+	//$thutu=$_POST['thutu'];
+	if(isset($_POST['them'])){
+		//them
+		$sql="insert into chitietsp (tensp,thutu,mota,gia,id_loaisp,hinhanh) values ('$tensp','$thutu','$mota','$gia','$loaisp','$hinhanh')";
+		//thuc thi voi csdl
+		mysqli_query($conn,$sql);
+		header('location:../../index.php?quanly=quanlychitietsp&ac=them');
+	}elseif(isset($_POST['sua'])){
+		//sua
+		//update ten table
+		if($hinhanh!=''){
+			$sql="update chitietsp set tensp='$tensp',thutu='$thutu',mota='$mota',gia='$gia',id_loaisp='$loaisp',hinhanh='$hinhanh' where id_sp='$id'";
+		}else{
+			$sql="update chitietsp set tensp='$tensp',thutu='$thutu',mota='$mota',gia='$gia',id_loaisp='$loaisp' where id_sp='$id'";
+		}
+		mysqli_query($conn,$sql);
+		header('location:../../index.php?quanly=quanlychitietsp&ac=sua&id='.$id);
+	}else{
+		//xoa
+		$sql="delete from chitietsp where id_sp='$id' ";
+		mysqli_query($conn,$sql);
+		header('location:../../index.php?quanly=quanlychitietsp&ac=them');
+	}
+?>
